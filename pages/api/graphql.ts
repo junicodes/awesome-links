b81819c18fc54c4e5cfe3b84c7c394beab398bf7
Obi Okechukwu Godwin <junicodesdev@gmail.com>
2021-11-17 16:19:48 +0100

    fix

--- pages/api/graphql.ts
+++ pages/api/graphql.ts
@@ -1,5 +1,5 @@
 import { ApolloServer } from "apollo-server-micro";
-import { typeDefs } from "../../graphql/schema";
+import { schema } from "../../graphql/schema";
 import { resolvers } from "../../graphql/resolvers";
 import { createContext } from "../../graphql/context";
 //Micro cors to enables us use appollo server studio without issues
@@ -8,7 +8,7 @@ import Cors from "micro-cors";
 const cors = Cors();
 
 const apolloServer = new ApolloServer({
-  typeDefs,
+  schema,
   resolvers,
   context: createContext,
 });
